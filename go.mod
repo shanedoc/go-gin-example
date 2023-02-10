7c881cbe17fa15102902b5c013b5875a487447b2
shanedoc <shanedoc@163.com>
2023-02-10 16:14:23 +0800

    重启服务

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
--- main.go
+++ main.go
@@ -3,21 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+	"syscall"
 
+	"github.com/fvbock/endless"
 	"github.com/shanedoc/go-gin-example/pkg/setting"
 	"github.com/shanedoc/go-gin-example/routes"
 )
 
 func main() {
-	router := routes.InitRouter()
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HttPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HttPPort)
+
+	server := endless.NewServer(endPoint, routes.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
-	log.Printf("[info] start http server %s", fmt.Sprintf(":%d", setting.HttPPort))
-	s.ListenAndServe()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
 }
